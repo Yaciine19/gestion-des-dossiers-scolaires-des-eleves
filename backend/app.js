a60e6d96abad2d641135d2644e818a240c9d26bb
yacine <yacineddd32@gmail.com>
2025-03-13 18:28:22 +0100

    nothing added

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,22 @@
 // import cookieParser from 'cookie-parser';
-import express from 'express';
-import { PORT } from './config/env.js';
-import userRouter from './routes/user.routes.js';
-import authRouter from './routes/auth.routes.js';
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the gestion des dossiers scolaires des eleves");
+});
 
-app.get('/', (req, res) => {res.send("Welcome to the gestion des dossiers scolaires des eleves")})
-
-
-app.listen(PORT, () => console.log(`the server is start on http://localhost:${PORT}`) )
+app.listen(PORT, async () => {
+  console.log(`the server is start on http://localhost:${PORT}`);
+  await connectToDatabase();
+});
 
 export default app;
